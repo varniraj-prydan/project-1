94477438440e375ef12dd4cf2bfa2c419ca7e6b2
Parth Dhaduk <parthdhaduk.prydan@gmail.com>
2023-10-11 16:42:54 +0530

    update het2.js

--- het2.js
+++ het2.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const het2 = () => {
     return (
-        <div>Ritik Component</div>
+        <div>
+            <RadioButton
+                dataList={[
+                    {
+                        label: "Excellent",
+                        value: "Excellent"
+                    },
+                    {
+                        label: "Above Average",
+                        value: "AboveAverage"
+                    },
+                    {
+                        label: "Avarage Below",
+                        value: "AvarageBelow"
+                    },
+                    {
+                        label: "Average Poor",
+                        value: "AveragePoor"
+                    }
+                ]}
+                fieldName={'Overall Event Rating'}
+                name={'eventRating'}
+                formik={formik}
+            />
+        </div>
     )
 }
 
